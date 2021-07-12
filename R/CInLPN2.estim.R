d7774e19cb86a1041fb6808f3e49358f2e3006b0
anarouanet <anarouanet@gmail.com>
2021-07-12 12:04:12 +0200

    good

--- R/CInLPN2.estim.R
+++ R/CInLPN2.estim.R
@@ -23,6 +23,7 @@ CInLPN2.estim <- function(K, nD, mapping.to.LP, data, if_link = if_link, DeltaT=
   cl <- match.call()
   #  non parall Optimisation 
   # package loading
+
   debug=0
   if(debug==1){
     browser()
@@ -83,8 +84,8 @@ CInLPN2.estim <- function(K, nD, mapping.to.LP, data, if_link = if_link, DeltaT=
                            x = data$x, z = data$z, q = data$q, nb_paraD = data$nb_paraD,
                            x0 = data$x0, z0 = data$z0, q0 = data$q0,tau = data$tau, tau_is=data$tau_is,
                            modA_mat = data$modA_mat, data_surv = as.matrix(data_surv), basehaz = ifelse(paras$basehaz=="Weibull", 0, 1), knots_surv = paras$knots_surv, 
-                           np_surv = paras$np_surv, survival = (!is.null(data_surv)), assoc =  paras$assoc, truncation = paras$truncation, 
-                           nE = data$nE, Xsurv1 = data$Xsurv1, Xsurv2 = data$Xsurv2, clustertype="FORK")
+                           np_surv = paras$np_surv, survival = (data$nE>0), assoc =  paras$assoc, truncation = paras$truncation, 
+                           nE = data$nE, Xsurv1 = as.matrix(data$Xsurv1), Xsurv2 = as.matrix(data$Xsurv2), clustertype="FORK")
     ,silent = FALSE)
     time=proc.time()-ptm
     h=floor(time[3]/3600)

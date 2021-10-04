d83f2ed78213945808754e78b47608d796ac2102
anarouanet <anarouanet@gmail.com>
2021-10-04 15:59:13 +0200

    new DataCreate

--- R/DataCreate.R
+++ R/DataCreate.R
@@ -1,5 +1,5 @@
 ##------------------------------------------------------------------------------------------------
-## fonction pour supprimer des ligne sans au moins une mesure des outcomes d'intérêt
+## fonction pour supprimer des lignes sans au moins une mesure des outcomes d'intérêt
 fifi <- function(x){
   d<-0 
   len <-length(na.omit(x))
@@ -26,87 +26,77 @@ one.outcome.at.least <- function(data, nom.subject, outcomes){
 }
 #=====================================================================================
 # fonction pour discr?tiser
-f_TempsDiscr <- function(VecAge, age0=65,Delta){
+f_TempsDiscr <- function(Time, Delta){
+  AxeT <-seq(from =min(Time), to = max(Time), by = Delta)
+  Time_d <- rep(0,length(Time))
   
-  tt <- round((max(VecAge, na.rm = TRUE) -age0)/Delta) + 2
-  AxeT <-rep(0,tt)
-  tau_i <- rep(0,length(VecAge))
-  for (i in 1:tt){
-    AxeT[i] <- age0 + Delta*(i-1)
-  }
-  
-  for(j in 1:length(VecAge)){
-    i <-1
-    tau_i[j] <- NA
-    if(!is.na(VecAge[j])){
-      tau_i[j] <- 0
-      while(VecAge[j] > AxeT[i]){
-        tau_i[j] <- i
+  for(j in 1:length(Time)){
+    i <-0
+    Time_d[j] <- NA
+    if(!is.na(Time[j])){
+      Time_d[j] <- 0
+      while(Time[j] > Delta*i){
         i <- i+1
       }
+      Time_d[j] <- i*Delta
     }
   }
-  return(tau_i)
+  return(Time_d)
 }
 
 #=====================================================================================
 # formatage des données brutes comme 3C pour retenir les outcomes, les covariables,
 # temps discrets d'observations
 # retourne un data frame
-DataCreate <-function(data, outcomes, predictors = NULL, subject, Visits, age0 = 0, age0.from.data = FALSE, Delta){
+DataCreate <-function(data, outcomes, predictors = NULL, subject, Time, age0 = 0, age0.from.data = FALSE, Delta){
   
   cl <- match.call()
   colnames<-colnames(data)
   if(!(subject%in%colnames))stop("Subject should be in colnames")
   
-  # v?rifier que les Visits sont dans le data.frame....
+  # v?rifier que les Time sont dans le data.frame....
   
-  # veriifer que length(outcomes) == length(Visits)...
-
-  if(length(outcomes) != length(Visits)){
-    stop("difference between number of outcomes and number of Visit variable")
+  # veriifer que length(outcomes) == length(Time)...
+  if(length(outcomes) != length(Time)){
+    stop("difference between number of outcomes and number of Time variable")
   }
   ##  pré-traitement du data frame
   data <- one.outcome.at.least(data, nom.subject= subject, outcomes = outcomes)
   
   K <- length(outcomes)
-  ##======= age0 initial référence====
-  if(age0.from.data==TRUE){
-    age0 <- min(data[,c(Visits)])
-  }
   T_k <-NULL
   nameTks <- NULL
-
   for(k in 1:K) {
-    nameTk <- paste("Visit", k, sep = "_")
+    nameTk <- paste(Time[k], k, sep = "_")
     nameTks <- c(nameTks,nameTk)
     #Discr?tisation du temps de suivi
-    T_k <- cbind(T_k,assign(nameTk, f_TempsDiscr(data[,Visits[k]], age0, Delta)))
+    T_k <- cbind(T_k,assign(nameTk, f_TempsDiscr(data[,Time[k]], Delta)))
   }
   T_k <- data.frame(T_k)
   colnames(T_k) <-nameTks
   data2 <-data.frame(cbind(data[,c(subject,outcomes, predictors)],T_k))
   #calcul du T max: Temps d'observation maximal
-  Tmax <- max((data2[,nameTks]),na.rm = TRUE)
+  # Tmax <- max((data2[,nameTks]),na.rm = TRUE)
   
   # merge
   data3 <- na.omit(data2[,c(subject, outcomes[1], predictors, nameTks[1])])
   #   data3 <- data2[,c(subject, outcomes[1], predictors, nameTks[1])]
+  #   data3 <- data2[,c(subject, outcomes[1], predictors, nameTks[1])]
   colname <- colnames(data3)
-  colname[which(colname==nameTks[1])] <- "Visit"
+  colname[which(colname==nameTks[1])] <- paste(Time[1],"d", sep = "_")
   colnames(data3) <- colname
   
   if(K>1){
     for(k in 2:K){
       data_k <- na.omit(data2[,c(subject, outcomes[k], predictors, nameTks[k])])
-      #       data_k <- data2[,c(subject, outcomes[k], predictors, nameTks[k])]
+      #             data_k <- data2[,c(subject, outcomes[k], predictors, nameTks[k])]
       # changement du nom de la colonne T_i en T pour faire le merging
       colname <- colnames(data_k)
-      colname[which(colname==nameTks[k])] <- "Visit"
+      colname[which(colname==nameTks[k])] <- paste(Time[1],"d", sep = "_")
       colnames(data_k) <- colname
       
-      data3 <- merge(data3,data_k, by=c(subject,predictors,"Visit"), all.x = TRUE, all.y = TRUE)
-      data3 <- data3[order(data3[,subject], data3$Visit), ]
+      data3 <- merge(data3,data_k, by=c(subject,predictors,paste(Time[1],"d", sep = "_")), all.x = TRUE, all.y = TRUE)
+      data3 <- data3[order(data3[,subject], data3[,c(paste(Time[1],"d", sep = "_"))]), ]
     }
   }
   return(data3)
